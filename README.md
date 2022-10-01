7c83a85f5137408ccdedbc77f1e5dbb04076affa
Tissyane <tissyane@gmail.com>
2022-10-01 12:35:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Projeto #15 - Boardcamp
+
+Introdução SQL
+
+Sistema de gestão de uma locadora de jogos de tabuleiro usando Banco de Dados Relacional (SQL)!
+
+Banco de Dados utilizado: Postgres
---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = "5000";
+
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
